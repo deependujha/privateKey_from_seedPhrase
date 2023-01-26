dd0dd302c5e84ab3e203de35dbdbd6ba883dcd73
deependujha <deependujha21@gmail.com>
2023-01-26 14:20:45 +0530

    added readme.md

--- index.ts
+++ index.ts
@@ -3,12 +3,24 @@ import { Wallet } from 'ethers';
 
 let seedPhrase = process.env.SEED_PHRASE || ''; // write your seed phrase here
 
-const mnemonicWallet = Wallet.fromMnemonic(seedPhrase);
+const firstAccount = Wallet.fromMnemonic(seedPhrase);
 
-const privateKey = mnemonicWallet.privateKey;
-const publicKey = mnemonicWallet.publicKey;
-const address = mnemonicWallet.address;
+let privateKey = firstAccount.privateKey;
+let publicKey = firstAccount.publicKey;
+let address = firstAccount.address;
 
-console.log(`your private key is: ${privateKey}`);
-console.log(`your public key is: ${publicKey}`);
-console.log(`your address is: ${address}`);
+console.log(`your first account private key is: ${privateKey}`);
+console.log(`your first account public key is: ${publicKey}`);
+console.log(`your first account address is: ${address}`);
+
+console.log("================================================");
+
+const secondAccount = Wallet.fromMnemonic(seedPhrase, `m/44'/60'/0'/0/1`);
+
+privateKey = secondAccount.privateKey;
+publicKey = secondAccount.publicKey;
+address = secondAccount.address;
+
+console.log(`your second account private key is: ${privateKey}`);
+console.log(`your second account public key is: ${publicKey}`);
+console.log(`your second account address is: ${address}`);
